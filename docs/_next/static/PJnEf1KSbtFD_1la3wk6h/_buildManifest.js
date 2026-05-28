5e6d8acd8e4ee5e44d3ced4779832a3dbefb1725
teo523 <teodoro.dan@gmail.com>
2026-05-28 13:54:23 +0100

    Fix GitHub Pages deployment

--- docs/_next/static/PJnEf1KSbtFD_1la3wk6h/_buildManifest.js
+++ docs/_next/static/PJnEf1KSbtFD_1la3wk6h/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/wivenhoe-music-trail//_next/:path+",
-        "destination": "/wivenhoe-music-trail/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
